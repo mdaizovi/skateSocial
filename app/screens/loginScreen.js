3c788ef7ad014dfee8056ceeb8532889e5a2091f
Mic <mdaizovi@gmail.com>
2021-12-10 17:29:50 +0100

    login looks nice, does nothing

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -1,112 +1,94 @@
-import React, {Component} from 'react';
-import { View, StyleSheet, FlatList, Text, TextInput, Button } from 'react-native';
+import React, {Component, useState} from 'react';
+import { View, StyleSheet, FlatList, Text, TextInput, Button, Image, TouchableOpacity,} from 'react-native';
 import appUserContext from '../context/appUserContext';
+import { StatusBar } from "expo-status-bar";
 
-export default class LoginScreen extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-     email: '',
-     password:''
-    }
-   }
-
-  static contextType = appUserContext;
-
-  render(){
-   return (
-     
+export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  return (
     <View style={styles.container}>
-    <View style={styles.view}>
-     <TextInput 
-      style={styles.input} 
-      onChangeText={(x)=>{this.setState({email: x})}}
-      placeholder="Email" 
-     />
-      <TextInput 
-      style={styles.input} 
-      onChangeText={(x)=>{this.setState({password: x})}}
-      placeholder="Password" 
-     />
-     <Text
-      style={styles.loginForm} 
-      onPress={()=>this.context.userLogIn(this.state.userLogIn)}
-      >
-       Log In
-      </Text>
-     </View>
+      <Image style={styles.image} source={require("../assets/icon.png")} />
+ 
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.link_button}>Forgot Password?</Text>
+      </TouchableOpacity>
 
 
-     <Text style={styles.title} >User</Text>
-     <FlatList 
-      data={this.context.user} 
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({item, index}) => {
-      return (
-       <View style={styles.rowcontainer}>
-        <Text style={styles.text}>{item}</Text>
-        <Text 
-         style={styles.delete}
-         onPress= {()=>{this.context.userLogOut()}}
-        >
-         delete
-        </Text>
-       </View>
-      )
-      }}
-      />
-     </View>
-    );
-   }
-};
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.link_button}>Register</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+ 
 const styles = StyleSheet.create({
- rowcontainer: {
-  flex: 1,
-  flexDirection: 'column',
-  marginLeft: 20,
-  marginRight: 20,
-  borderBottomWidth: 1,
-  borderBottomColor: 'gray'
- },
- title: {
-  marginTop:20,
-  marginLeft: 20,
-  fontSize: 30,
- },
- text: {
-  padding: 10,
-  fontSize: 20
- },
- delete: {
-  alignSelf: 'flex-end',
-  padding: 8,
-  fontSize: 15,
- },
- container: {
-  flex: 1,
-  flexDirection:'column',
- },
- view: {
-  flex: 1,
-  width: '100%',
-  top: 0,
-  position: 'absolute',
-  flexDirection: 'row'
- },
- input: {
-  width:'30%',
-  borderRadius: 30,
-  borderWidth:1,
-  padding: 5,
-  marginLeft: 10,
-  fontSize: 18,
-  height:50
- },
- loginForm: {
-  fontSize: 20,
-  padding: 5,
-  position: 'absolute',
-  right: '10%',
-  //top:10,
- }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+ 
+  image: {
+    marginBottom: 40,
+  },
+ 
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+ 
+    alignItems: "center",
+  },
+ 
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+ 
+  link_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+ 
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 30,
+    backgroundColor: "#FF1493",
+  },
 });
